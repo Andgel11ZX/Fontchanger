724ebdd3d3ec505f9d238e303e24e13367897753
JohnFawkes <33303104+JohnFawkes@users.noreply.github.com>
2020-07-15 08:31:29 -0400

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -1,13 +1,13 @@
-mkdir -p /storage/emulated/0/Fontchanger/logs
+#mkdir -p /storage/emulated/0/Fontchanger/logs
 #mkdir -p /sbin/.$MODID/logs
-exec 2>/storage/emulated/0/Fontchanger/logs/Fontchanger-install-verbose.log
-set -euxo pipefail
-mkdir $TMPDIR/tools
-unzip -o "$ZIPFILE" 'module.prop' -d $MODPATH 2>&1
-unzip -o "$ZIPFILE" 'tools/*' -d $TMPDIR 2>&1
-mv $TMPDIR/tools/busybox-$ARCH32 $TMPDIR/tools/busybox 2>&1
-chmod 0755 $TMPDIR/tools/busybox
-SKIPUNZIP=1
+#exec 2>/storage/emulated/0/Fontchanger/logs/Fontchanger-install-verbose.log
+#set -euxo pipefail
+#mkdir $TMPDIR/tools
+#unzip -o "$ZIPFILE" 'module.prop' -d $MODPATH 2>&1
+#unzip -o "$ZIPFILE" 'tools/*' -d $TMPDIR 2>&1
+#mv $TMPDIR/tools/busybox-$ARCH32 $TMPDIR/tools/busybox 2>&1
+#chmod 0755 $TMPDIR/tools/busybox
+#SKIPUNZIP=1
 
 set_busybox() {
   if [ -x "$1" ]; then
@@ -20,32 +20,34 @@ set_busybox() {
     _bb=$1
   fi
 }
-_busybox=false
+#_busybox=false
 
-if $_busybox; then
-  true
-elif [ -d /sbin/.magisk/modules/busybox-ndk ]; then
-  BUSY=$(find /sbin/.magisk/modules/busybox-ndk/system/* -maxdepth 0 | sed 's#.*/##')
-  for i in $BUSY; do
-    PATH=/sbin/.magisk/modules/busybox-ndk/system/$i:$PATH
-    _bb=/sbin/.magisk/modules/busybox-ndk/system/$i/busybox
-  done
-elif [ -f /sbin/.magisk/modules/ccbins/system/bin/busybox ]; then
-  PATH=/sbin/.magisk/modules/ccbins/system/bin:$PATH
-  _bb=/sbin/.magisk/modules/ccbins/system/bin/busybox
-elif [ -f /sbin/.magisk/modules/ccbins/system/xbin/busybox ]; then
-  PATH=/sbin/.magisk/modules/ccbins/system/xbin:$PATH
-  _bb=/sbin/.magisk/modules/ccbins/system/xbin/busybox
-elif [ -f $TMPDIR/tools/busybox ]; then
-  PATH=$TMPDIR/tools:$PATH
-  _bb=$TMPDIR/tools/busybox
-elif [ -d /sbin/.magisk/busybox ]; then
-  PATH=/sbin/.magisk/busybox:$PATH
-  _bb=/sbin/.magisk/busybox/busybox
-fi
+#if $_busybox; then
+#  true
+#elif [ -d /sbin/.magisk/modules/busybox-ndk ]; then
+#  BUSY=$(find /sbin/.magisk/modules/busybox-ndk/system/* -maxdepth 0 | sed 's#.*/##')
+#  for i in $BUSY; do
+#    PATH=/sbin/.magisk/modules/busybox-ndk/system/$i:$PATH
+#    _bb=/sbin/.magisk/modules/busybox-ndk/system/$i/busybox
+#  done
+#elif [ -f /sbin/.magisk/modules/ccbins/system/bin/busybox ]; then
+#  PATH=/sbin/.magisk/modules/ccbins/system/bin:$PATH
+#  _bb=/sbin/.magisk/modules/ccbins/system/bin/busybox
+#elif [ -f /sbin/.magisk/modules/ccbins/system/xbin/busybox ]; then
+#  PATH=/sbin/.magisk/modules/ccbins/system/xbin:$PATH
+#  _bb=/sbin/.magisk/modules/ccbins/system/xbin/busybox
+#elif [ -f $TMPDIR/tools/busybox ]; then
+#  PATH=$TMPDIR/tools:$PATH
+#  _bb=$TMPDIR/tools/busybox
+#elif [ -d /sbin/.magisk/busybox ]; then
+#  PATH=/sbin/.magisk/busybox:$PATH
+#  _bb=/sbin/.magisk/busybox/busybox
+#fi
 
-set_busybox $_bb
-[ $? -ne 0 ] && exxit $?
-ui_print " - Downloading Installer Script"
-wget -O $TMPDIR/installer.sh https://github.com/johnfawkes/fontchanger-scripts/raw/master/installer.sh 2>/dev/null
-. $TMPDIR/installer.sh
+#set_busybox $_bb
+#[ $? -ne 0 ] && exxit $?
+#ui_print " - Downloading Installer Script"
+#wget -O $TMPDIR/installer.sh https://github.com/johnfawkes/fontchanger-scripts/raw/master/installer.sh 2>/dev/null
+#. $TMPDIR/installer.sh
+
+abort " BROKEN, WILL FIX ASAP"
